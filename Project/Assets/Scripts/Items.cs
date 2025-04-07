c7d567486aa59230f298fa3e3686279f4569c039
Rajiya Sultana <rssumu04@gmail.com>
2025-04-07 23:25:52 +0600

    Fixed callback issue
    Rename start to awake method in "Items.cs"

--- Project/Assets/Scripts/Items.cs
+++ Project/Assets/Scripts/Items.cs
@@ -3,74 +3,71 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 using SimpleJSON;
-using UnityEngine.UI;
 using TMPro;
 
 public class Items : MonoBehaviour
 {
-    Action<string> _creatItemsCallback;
-    
-    void Start()
+    private Action<string> _createItemsCallback;
+
+    private void Awake()
     {
         Debug.Log("Items script started!");
-        _creatItemsCallback = (jsonArrayString) =>
+        _createItemsCallback = (jsonArrayString) =>
         {
             Debug.Log("Callback received JSON response!");
             StartCoroutine(CreateItemsRoutine(jsonArrayString));
         };
-        CreateItems();
     }
 
     public void CreateItems()
     {
-        Debug.Log("CreatItems() called!");
+        Debug.Log("CreateItems() called!");
         string userId = ObjectHolder.Instance.UserInfo.UserID;
-        StartCoroutine(ObjectHolder.Instance.Web.GetItemsIDs(userId, _creatItemsCallback));
-        
+        StartCoroutine(ObjectHolder.Instance.Web.GetItemsIDs(userId, _createItemsCallback));
     }
 
-    IEnumerator CreateItemsRoutine(string jsonArrayString)
+    private IEnumerator CreateItemsRoutine(string jsonArrayString)
     {
-        //parsing json array string as an array
-        
         JSONArray jsonArray = JSON.Parse(jsonArrayString) as JSONArray;
+
+        if (jsonArray == null)
+        {
+            Debug.LogError("Invalid JSON array received.");
+            yield break;
+        }
 
         for (int i = 0; i < jsonArray.Count; i++)
         {
-            //create local variables
             bool isDone = false;
             string itemId = jsonArray[i].AsObject["itemID"];
             JSONObject itemInfoJson = new JSONObject();
 
-            //create a callback to get information from web.cs
             Action<string> getItemInfoCallback = (itemInfo) =>
             {
-                Debug.Log("Item Info JSON: " + itemInfo);
-                isDone = true;
                 JSONArray tempArray = JSON.Parse(itemInfo) as JSONArray;
-                itemInfoJson = tempArray[0].AsObject;
+                if (tempArray != null && tempArray.Count > 0)
+                {
+                    itemInfoJson = tempArray[0].AsObject;
+                }
+                else
+                {
+                    Debug.LogWarning("Invalid item info received for itemID: " + itemId);
+                }
+                isDone = true;
             };
 
             StartCoroutine(ObjectHolder.Instance.Web.GetItem(itemId, getItemInfoCallback));
 
-            //Wait untill the callback is called from WEB (info finisherd downloading)
-            yield return new WaitUntil(() => isDone == true);
+            yield return new WaitUntil(() => isDone);
 
-            
-            //Instansiate gameobject (item prefebs)
             GameObject item = Instantiate(Resources.Load("Prefabs/Item") as GameObject);
-            item.transform.SetParent(this.transform);
+            item.transform.SetParent(this.transform, false);
             item.transform.localScale = Vector3.one;
             item.transform.localPosition = Vector3.zero;
 
-            //Fill information
             item.transform.Find("Name").GetComponent<TMP_Text>().text = itemInfoJson["Name"];
             item.transform.Find("Price").GetComponent<TMP_Text>().text = itemInfoJson["Price"];
             item.transform.Find("Description").GetComponent<TMP_Text>().text = itemInfoJson["Description"];
-
-            //continue to the next item
-
-
         }
     }
 }
